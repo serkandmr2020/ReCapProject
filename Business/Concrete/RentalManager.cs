e4eb2ffa1e48c0058122cf097713cecb8715cfeb
Serkan Demir <serkandmr2020@gmail.com>
2022-04-27 10:54:06 +0300

    27.04.2022

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,70 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+
+        public IResult Add(Rental rental)
+        {
+            if (CheckReturnStatus(rental.CarId))
+            {
+                _rentalDal.Add(rental);
+                return new SuccessResult(Messages.RentalAdded);
+            }
+
+            return new ErrorResult(Messages.CarToRentNotReady);
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+            return new SuccessResult(Messages.RentalDeleted);
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            var result = _rentalDal.GetAll();
+            return new SuccessDataResult<List<Rental>>(result,Messages.RentalsListed);
+        }
+
+        public IDataResult<Rental> GetById(int id)
+        {
+            var result = _rentalDal.Get(f=>f.Id==id);
+            return new SuccessDataResult<Rental>(result, Messages.RentalListedById);
+        }
+
+        public IResult Update(Rental rental)
+        {
+            _rentalDal.Update(rental);
+            return new SuccessResult(Messages.RentalUpdated);
+        }
+
+        private bool CheckReturnStatus(int carId)
+        {
+            var rental = _rentalDal.Get(f => f.CarId == carId);
+
+            if (rental!=null&&rental.ReturnDate==null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -12,7 +12,7 @@ namespace Business.Constants
         public static string BrandDeleted = "Marka silindi";
         public static string BrandsListed = "Markalar listelendi";
         public static string BrandListedById = "Id ye göre marka getirildi";
-        public static string BrandUpdated = "";
+        public static string BrandUpdated = "Marka güncellendi";
 
         public static string CarAdded = "Araba eklendi";
         public static string CarNameInvalid="Araba adı geçersiz";
@@ -28,5 +28,25 @@ namespace Business.Constants
         public static string ColorUpdated = "Renk güncellendi";
         public static string ColorsListed = "Renkler listelendi";
         public static string ColorListedById = "Id ye göre renk getirildi";
+
+        public static string UserAdded = "Kullanıcı eklendi";
+        public static string UserDeleted = "Kullanıcı silindi";
+        public static string UsersListed = "Kullanıcı güncellendi";
+        public static string UserListedById = "Kullanıcılar listelendi";
+        public static string UserUpdated = "Id ye göre kullanıcı getirildi";
+
+        public static string RentalAdded = "Kiralama eklendi";
+        public static string CarToRentNotReady = "Kiralanmak istenen araç henüz teslim edilmemiş";
+        public static string RentalDeleted = "Kiralama silindi";
+        public static string RentalUpdated = "Kiralama güncellendi";
+        public static string RentalsListed = "Kiralamalar listelendi";
+        public static string RentalListedById = "Id ye göre kiralama getirildi";
+
+        public static string CustomerAdded= "Müşteri eklendi";
+        public static string CustomerDeleted = "Müşteri silindi";
+        public static string CustomerUpdated = "Müşteri güncellendi";
+        public static string CustomersListed = "Müşteriler listelendi";
+        public static string CustomerListedById = "Id ye göre müşteri getirildi";
+        
     }
 }
